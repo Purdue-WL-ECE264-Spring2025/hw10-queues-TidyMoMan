347f29b727ab57e817ea4a7f6d6d85ed74f13be2
TidyMoMan <mosesdilts@gmail.com>
2025-04-03 18:30:58 -0400

    working on it

--- linked_list.c
+++ linked_list.c
@@ -2,22 +2,59 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node* new_node(size_t value) {
+  struct list_node* newNode = malloc(sizeof(struct list_node*));
+  newNode->next = NULL;
+  newNode->value = value;
+  return newNode;
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node* newNode = new_node(value); //create a new node with value set
+  //does head->next exist???
+  newNode = list->head;
+  list->head = newNode;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node* tempHead = list->head;
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+  while(tempHead->next != NULL){ //go until the next node is null, meaning we have reached the end of the list
+    tempHead = tempHead->next;
+  }
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+  tempHead->next = new_node(value); //make a new node at the end of the list.
+}
 
-void free_list(struct linked_list list) {}
+size_t remove_from_head(struct linked_list *list) {
+  struct list_node* toDelete = list->head;
+  list->head = list->head->next;
+  free(toDelete);
+  return 0;
+}
 
-// Utility function to help you debugging, do not modify
+size_t remove_from_tail(struct linked_list *list) {
+  struct list_node* tempHead = list->head;
+
+  while(tempHead->next != NULL){ //go until the next node is null, meaning we have reached the end of the list
+    tempHead = tempHead->next;
+  }
+ 
+  free(tempHead->next);
+  return 0;
+}
+
+void free_list(struct linked_list list) {
+  while(list.head->next != NULL){
+    list.head = list.head->next;
+    remove_from_head(&list); //set list to itself without the first node!
+  }
+}
+
+// Utility function to help you when debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
   fprintf(fp, "[ ");
-  for (struct list_node *cur = list.head; cur != NULL; cur = cur->next) {
+  for (struct list_node *cur = list.head; cur != NULL; cur = cur->next){
     fprintf(fp, "%zu ", cur->value);
   }
   fprintf(fp, "]\n");
--- queue.c
+++ queue.c
@@ -1,8 +1,18 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+  //insert at the head
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+  insert_at_head();
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+struct game_state dequeue(struct queue *q){
+  //remove from the tail
+
+  return (struct game_state){0};
+}
+
+int number_of_moves(struct game_state start){ 
+  
+  return 0; }
